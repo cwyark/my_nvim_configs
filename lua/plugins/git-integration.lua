e8e3d5fc685d8d28f128afa35af216bd4b4cb417
YuSheng <chester@cwyark.me>
2025-03-06 22:44:14 +0800

    remove used modules

--- lua/plugins/git-integration.lua
+++ lua/plugins/git-integration.lua
@@ -4,12 +4,4 @@ return {
     version = "*",
     opts = true,
   },
-  {
-    "rbong/vim-flog",
-    lazy = true,
-    cmd = { "Flog", "Flogsplit", "Floggit" },
-    dependencies = {
-      "tpope/vim-fugitive",
-    },
-  },
 }
