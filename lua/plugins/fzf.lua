8bfc5bfc96d7ed63b2212664bae926bd02503b31
YuSheng <chester@cwyark.me>
2025-03-13 23:14:53 +0800

    update fzf key mapping

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -5,9 +5,11 @@ return {
       local fzf = require("fzf-lua")
       local actions = fzf.actions
 
-      -- Update key mapping for toggle_hidden
       opts.files.actions["ctrl-h"] = { actions.toggle_hidden }
       opts.files.actions["ctrl-i"] = { actions.toggle_ignore }
+
+      opts.grep.actions["ctrl-h"] = { actions.toggle_hidden }
+      opts.grep.actions["ctrl-i"] = { actions.toggle_ignore }
 
       return opts
     end,
