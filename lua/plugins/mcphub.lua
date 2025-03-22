258d083a3567adc1b96d6727396d69526b92072b
YuSheng <chester@cwyark.me>
2025-03-22 22:01:22 +0800

    update mcp server configs

--- lua/plugins/mcphub.lua
+++ lua/plugins/mcphub.lua
@@ -1,7 +1,8 @@
 return {
   {
     "ravitemer/mcphub.nvim",
-    version = "*",
+    -- version = "v3.5.0",
+    version = false,
     dependencies = {
       "nvim-lua/plenary.nvim",
     },
@@ -9,7 +10,7 @@ return {
     opts = {
       port = 32400,
       config = vim.fn.stdpath("config") .. "/mcpservers.json",
-      shutdown_delay = 0, -- Wait 0ms before shutting down server after last client exits
+      shutdown_delay = 200, -- Wait 0ms before shutting down server after last client exits
       log = {
         level = vim.log.levels.WARN,
         to_file = false,
