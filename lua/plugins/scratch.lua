df202e86187bff4a7572b2fa4dce9f2ee3333f9b
YuSheng <chester@cwyark.me>
2025-06-09 22:15:52 +0800

    update scratch

--- lua/plugins/scratch.lua
+++ lua/plugins/scratch.lua
@@ -1,4 +1,51 @@
 return {
-  "LintaoAmons/scratch.nvim",
-  event = "VeryLazy",
+  {
+    "LintaoAmons/scratch.nvim",
+    version = "*",
+    config = {
+      filetypes = { "py", "rs", "cpp" },
+      filetype_details = {
+        rs = {
+          requireDir = true,
+          content = { "", "", "fn main(){", "", "}" },
+          cursor = {
+            location = { 4, 2 },
+            insert_mode = true,
+          },
+        },
+        cpp = {
+          content = { "", "", "int main() {", "", "}" },
+          cursor = {
+            location = { 4, 2 },
+            insert_mode = true,
+          },
+        },
+      },
+    },
+    keys = {
+      {
+        "<leader>fsn",
+        "<cmd>Scratch<cr>",
+        desc = "New Scrach file",
+      },
+      {
+        "<leader>fsN",
+        "<cmd>ScratchWithName<cr>",
+        desc = "New Scrach file with file name",
+      },
+      {
+        "<leader>fsf",
+        "<cmd>ScratchOpenFzf<cr>",
+        desc = "Fuzzy find scrach file.",
+      },
+    },
+  },
+  {
+    "folke/which-key.nvim",
+    opts = {
+      spec = {
+        { "<leader>fs", group = "scratch", icon = "󱦹" },
+      },
+    },
+  },
 }
