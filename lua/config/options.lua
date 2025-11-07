20e3585a9e2b24efbb3f6a67c601b405aa8d45d1
YuSheng <chester@cwyark.me>
2025-11-07 17:27:36 +0800

    remove unused setting

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,35 +4,6 @@
 
 vim.opt.laststatus = 3
 
--- When running on the remote Neovim host, prepend `remote-tools/<os>-<arch>/bin` to PATH
--- so that locally-prepared tools (lazygit, yazi, node/npm wrappers) are usable in remote sessions.
--- Place tools under: stdpath('data')/remote-tools/<os>-<arch>/bin
--- Example: ~/.local/share/nvim/remote-tools/linux-x86_64/bin/lazygit
-if vim.g.remote_neovim_host then
-  local uname = vim.uv and vim.uv.os_uname() or vim.loop.os_uname()
-  local osname = (uname.sysname or ""):lower()
-  -- Normalize OS name to directory convention
-  if osname == "darwin" then
-    osname = "macos"
-  elseif osname == "linux" then
-    osname = "linux"
-  end
-
-  local arch = uname.machine or ""
-  -- Normalize arch name
-  if arch == "aarch64" then
-    arch = "arm64"
-  end
-
-  local data = vim.fn.stdpath("data")
-  local tools_bin = table.concat({ data, "remote-tools", (osname .. "-" .. arch), "bin" }, "/")
-
-  if vim.fn.isdirectory(tools_bin) == 1 then
-    -- Prepend the tools bin directory to the PATH
-    vim.env.PATH = tools_bin .. ":" .. (vim.env.PATH or "")
-  end
-end
-
 vim.o.clipboard = "unnamedplus"
 
 local function paste()
